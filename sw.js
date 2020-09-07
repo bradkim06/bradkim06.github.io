60e92646219d9e7ecfee2174bbee0b3d70fef3a7
bradkim06 <bradkim06@gmail.com>
2020-09-07 13:58:31 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2524233934f4f7c72ee1.js"
+    "url": "webpack-runtime-9c86d2aaeba173d24255.js"
   },
   {
     "url": "styles.07c309dccf6fa0c2698a.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "03a96c02b6518c4d81bfde85d7705bfd"
+    "revision": "6644227d3f922db40d815d5fe8fdf2ec"
   },
   {
     "url": "google-fonts/s/opensans/v17/mem5YaGs126MiZpBA-UN_r8OUuhp.woff2",
